2ef744ad4b455721064b0647a1683135fb7f895d
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-06-30 12:21:46 +0200

    Show promo content when hovering of promo heading with js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,20 @@ const handleToggle = () => {
 }
 
 setInterval(handleToggle, 5000)
+
+// promotion
+
+const promoHeading = document.querySelector('.promo-heading')
+
+const addPromoContent = () => {
+  const promoContent = '<p class="promoContent">Get 10 % discount on all Japanese maples</p>'
+  promoHeading.insertAdjacentHTML('beforeend', promoContent)
+}
+
+const hidePromoContent = e => {
+  const promoContent = e.target.querySelector('.promoContent')
+  promoHeading.removeChild(promoContent)
+}
+
+promoHeading.addEventListener('mouseenter', addPromoContent)
+promoHeading.addEventListener('mouseleave', hidePromoContent)
